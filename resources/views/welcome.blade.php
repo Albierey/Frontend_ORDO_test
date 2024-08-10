46f1684e4e4c96fab8c0360a3da7a3fd33d748d1
Albierey <86662918+Albierey@users.noreply.github.com>
2024-08-10 15:50:45 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Frontend ORDO</title>
+    @vite(['resources/css/app.scss', 'resources/js/app.js'])
+        
+</head>
+<body>
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <img src="assets/Frame 5.png">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">About Green Haven</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Event Details</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Our Sponsors</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Leaderboard</a>
+        </li>
+      </ul>
+      <button class="btn btn-outline-success" type="submit">Plant a Mangrove</button>
+    </div>
+  </div>
+</nav>
+<!-- End Navbar -->
+<!-- Section 1 -->
+<div class="container">
+  <div class="row align-items-center">
+    <div class="col-7"><br>
+      <div class="border">Green Haven Project - Mangrove</div><br>
+      <h2 class="heading">Green Horizons Await, Join Us in
+      Planting 10,000 Mangroves!</h2> <br>
+      <img class="pohon" src="assets/Frame 34.png">
+      <a href="#">
+        <img class="pohon" src="assets/Frame 4.png">
+      </a><br>
+      <img class="pohon" src="assets/Frame 35.png"><br><br>
+      <img class="pohon" src="assets/Rectangle 1.png">
+    </div>
+    <div class="col-5"><br><br>
+    <h2 class="heading2">Leaderboard</h2>
+    <div class="mostd" >Most Donation</div>
+    <div class="mostr" >Most Recent</div>
+    <img class="pohon" src="assets/Frame 68.png">
+    </div>
+    <img class="pohon" src="assets/Frame 53.png">
+  </div>
+</div>
+<!-- End Section 1 -->
+<!-- Section 2 -->
+<div class="container center">
+  <div class="row">
+    <div class="col">
+      Column2 
+      <img src="assets/Frame 41.png">  
+    </div>
+  </div>
+</div>
+
+
+    <!-- Bootstrap JavaScript components can be used here -->
+</body>
+</html>
